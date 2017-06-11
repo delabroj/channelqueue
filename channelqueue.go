d555b545fbb491d87b6c272da67133f51e1dddff
delabroj <delabroj@users.noreply.github.com>
2017-06-11 13:55:47 -0600

    make safe for concurrent use

--- channelqueue.go
+++ channelqueue.go
@@ -2,10 +2,13 @@
 // to a maximum length.
 package channelqueue
 
+import "sync"
+
 // Queue holds the current channel and the maximum length of the channel.
 type Queue struct {
 	front   chan int
 	maxSize int
+	mux     sync.Mutex
 }
 
 // MakeQueue returns a pointer to an initialized queue struct.
@@ -25,6 +28,8 @@ func (e ErrOverflow) Error() string {
 // Enqueue adds the value to the queue's channel. If the channel is full, the
 // channel is replaced with another channel twice as large until maxSize is reached.
 func (q *Queue) Enqueue(n int) error {
+	q.mux.Lock()
+	defer q.mux.Unlock()
 	select {
 	case q.front <- n:
 	default:
@@ -57,6 +62,8 @@ func (e ErrUnderflow) Error() string {
 
 // Dequeue returns the next item in the queue, or an error if the queue is empty.
 func (q *Queue) Dequeue() (int, error) {
+	q.mux.Lock()
+	defer q.mux.Unlock()
 	select {
 	case v := <-q.front:
 		return v, nil
